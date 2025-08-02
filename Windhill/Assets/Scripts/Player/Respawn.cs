4abd9d882748b8f8f5c12b41aba56a9f1d1e203e
Enzo Rafael <enzoricarteaa@gmail.com>
2025-08-02 12:26:53 -0300

    FIX

--- Windhill/Assets/Scripts/Player/Respawn.cs
+++ Windhill/Assets/Scripts/Player/Respawn.cs
@@ -4,10 +4,18 @@ public class Respawn : MonoBehaviour
 {
     public Transform lastSpawnPoint;
 
-    void OnTriggerEnter(Collider other)
+    /*void OnCollisionEnter(Collision other)
     {
-        if(other.CompareTag("Void")){
+        Debug.Log("Colidiu");
+        if(other.gameObject.CompareTag("Void")){
+
             transform.position = lastSpawnPoint.position;
         }
+    }*/
+    public void RepawnPlayer()
+    {
+        GetComponent<CharacterController>().enabled = false;
+        transform.position = lastSpawnPoint.position;
+        GetComponent<CharacterController>().enabled = true;
     }
 }
